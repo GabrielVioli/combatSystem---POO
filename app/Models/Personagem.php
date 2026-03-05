d45d530a8129a428048ad62f036f1ccdff5b066e
Gabriel <gabrielgvo16@gmail.com>
2026-03-05 15:32:03 -0300

    feat: estrutura inicial do MVC

---
+++ app/Models/Personagem.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+namespace App\Models;
+
+abstract class Personagem
+{
+    protected string $nome;
+    protected int $vida;
+    protected int $ataque;
+
+    public function __construct(string $nome, int $vida, int $ataque)
+    {
+        $this->nome = $nome;
+        $this->vida = $vida;
+        $this->ataque = $ataque;
+    }
+}
